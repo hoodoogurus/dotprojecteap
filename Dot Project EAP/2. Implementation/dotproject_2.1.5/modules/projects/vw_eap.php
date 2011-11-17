1d1e758f0e51b640da9c86de6b4dff87303434be
renan2703 <renan2703@gmail.com>
2011-11-17 22:45:34 +0000

    Alterações do modulo eap.

--- Dot Project EAP/2. Implementation/dotproject_2.1.5/modules/projects/vw_eap.php
+++ Dot Project EAP/2. Implementation/dotproject_2.1.5/modules/projects/vw_eap.php
@@ -1,17 +1,18 @@
-<?php /* PROJECTS $Id: vw_forums.php*/
+<?php 
 if (!defined('DP_BASE_DIR')) {
   die('You should not access this file directly.');
 }
 
 GLOBAL $AppUI, $project_id;
-// Forums mini-table in project view action
+
+$cont = 0;
+
+//Busca valores da eap cadastrados no banco de dados
 $q  = new DBQuery;
 $q->addTable('eap');
 $q->addQuery("id,nome,linha,coluna");
-/*$q->addJoin('projects', 'p', 'project_id = forum_project');
-$q->addWhere("forum_project = $project_id");
-$q->addOrder('forum_project, forum_name');*/
 $rc = $q->exec();
+
 ?>
 
 <table width="100%" border="0" cellpadding="2" cellspacing="1" class="tbl">
@@ -22,20 +23,15 @@ $rc = $q->exec();
 	<th nowrap><?php echo $AppUI->_('Last Post');?></th>
 </tr>
 <?php
-while ($row = db_fetch_assoc($rc)) { ?>
+//Listando valores cadastrados no BD
+
+while ($row = db_fetch_assoc($rc)) { $cont = $cont+1?>
 <tr>
 	<td nowrap align=center>
-<?php
-	if ($row["forum_owner"] == $AppUI->user_id) { ?>
-		<A href="./index.php?m=forums&a=addedit&forum_id=<?php echo $row["forum_id"];?>"><img src="./images/icons/pencil.gif" alt="expand forum" border="0" width=12 height=12></a>
-<?php } ?>
 	</td>
-	<td nowrap><A href="./index.php?m=forums&a=viewer&forum_id=<?php echo $row["forum_id"];?>"><?php echo $row["forum_name"];?></a></td>
-	<td nowrap><?php echo $row["forum_message_count"];?></td>
-	<td nowrap>
-		<?php echo (intval($row["forum_last_date"]) > 0) ? $row["forum_last_date"] : 'n/a'; ?>
-	</td>
+	<td nowrap><?php echo $row["nome"];?></td>
 </tr>
+	<?php if($row["id"]>$var) $var = $row["id"]; ?>
 <tr>
 	<td></td>
 	<td colspan=3><?php echo $row["forum_description"];?></td>
@@ -43,10 +39,53 @@ while ($row = db_fetch_assoc($rc)) { ?>
 <?php }
 $q->clear();
 ?>
-</table>
-<input type="button" value ="<?php echo $AppUI->_('new EAP'); ?>" class="button" onClick="window.alert('Nova EAP')">
-<input type="button" value ="<?php echo $AppUI->_('Cadastrar'); ?>" class="button" onClick="window.alert('Cadastro info')">
-<input type="button" value ="<?php echo $AppUI->_('Alterar'); ?>" class="button" onClick="window.alert('Alteração info')">
-<input type="button" value ="<?php echo $AppUI->_('Excluir'); ?>" class="button" onClick="window.alert('Excluir info')">
-<input type="button" value ="<?php echo $AppUI->_('Recuperar'); ?>" class="button" onClick="window.alert('Recuperar info')">
 
+<?php
+
+//Insere uma linha
+	$q  = new DBQuery;
+	$q->addTable('eap');
+	//$sql = "INSERT INTO `letsnight_1`.`dotp_eap` (`id`, `nome`, `linha`, `coluna`) VALUES (NULL, 'Build 3', '2', '3')";
+	$q->addQuery("id,nome,linha,coluna");
+	$q->addInsert(nome,'Build '.($var+1));
+	$q->addInsert(linha,2);
+	$q->addInsert(coluna,3);
+	$q->prepareInsert();
+	$q->exec();
+	$q->clear();
+?>
+
+<?php 
+//Altera valor da linha
+$q  = new DBQuery;
+$q->addTable('eap');
+$q->addQuery("id,nome,linha,coluna");
+$q->addUpdate(nome,'Build'.($var-5));
+$q->addWhere("id = 58");
+$q->prepareUpdate();
+$q->exec();
+$q->clear();
+?>
+
+<?php
+//Delete uma linha
+$var = $var-1;
+$q  = new DBQuery;
+$q->addTable('eap');
+$q->setDelete('eap');
+$q->addWhere("id = $var");
+$q->prepareDelete();
+$q->exec();
+$q->clear();
+?>
+
+
+
+</table>
+
+<form method="get" action="./tratarBotoesPHP.php">
+<input type="hidden" name="m" value="forums" />
+<input type="hidden" name="a" value="addedit" />
+<input type="hidden" name="forum_project" value="<?php echo $project_id; ?>" />
+<input type="submit" value="<?php echo $AppUI->_('new forum'); ?>" class="button" />
+</form>
